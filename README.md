4e8e6b553a496f0b70c36b13e045be2bcd651d85
Wojciech <kawka.wojciech.kontakt@gmail.com>
2026-03-20 16:48:20 +0100

    add readme and project description

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# DevOps Learning Project
-This repository contains my DevOps learning journey.
-Technologies:
-Docker
-CI/CD
-AWS
-Terraform
+Devops Docker Compose App
+
+Simple devops project demonstrating: Docker, Docker Compose, Flask API, PostgreSQL, Enviroment variables, persistent volumes
+
+Stack: Python - flask, docker, docker compose, postgresql.|
+
+Run project: bash
+docker compose up --build
--- app/app.py
+++ app/app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+import psycopg2
+import os
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def home():
-    return "DevOps project is running 🚀"
+    return "DevOps Compose with ENV is running 🚀"
+
+@app.route("/db")
+def db_test():
+    conn = psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        port=os.getenv("POSTGRES_PORT")
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"Database version: {db_version}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
